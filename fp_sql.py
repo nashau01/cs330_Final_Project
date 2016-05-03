8a1cad5f42aa93a2e03d467b174d50352fe5475d
austinnash <nashau01@luther.edu>
2016-05-03 11:54:22 -0500

    1) Clarifications added to project_requirements 2) Some updates to fp_sql.py (database still not working, heroes.db file was copied and pasted manually).

--- fp_sql.py
+++ fp_sql.py
@@ -2,12 +2,20 @@ from flask import Flask, request, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import date
+
+engine = create_engine('sqlite:///heroes.db',echo=True)
+
 from datetime import date
 
 app = Flask(__name__)
 app.debug = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////todo_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////heroes.db'
 db = SQLAlchemy(app)
 
 class Hero(db.Model):
@@ -20,7 +28,14 @@ class Hero(db.Model):
     #due = db.Column(db.Date)
     #done = db.Column(db.Boolean)
 
-#class
+"""
+class User(db.Model):
+    __tablename__ = 'user'
+    username = db.Column(db.String, primary_key=True)
+    heroes_owned = db.Column(?) #Many
+    heroes_favorited = db.Column(?) #Many
+"""
+
 
 #Base.metadata.create_all(engine)
 db.create_all()
@@ -38,33 +53,37 @@ if len(results) == 0:
     session.commit()
 """
 if len(results) == 0:
-    task = Todo(task = 'example for 330', priority = 'High', due = date(2016,3,28), done = False)
-    db.session.add(task)
-    task = Todo(task = 'another example for 330', priority = 'High', due = date(2016,4,28), done = False)
-    db.session.add(task)
+    zagara = Hero(name = 'Zagara')
+    db.session.add(zagara)
+    uther = Hero(name = 'Uther')
+    db.session.add(uther)
 
     db.session.commit()
 
 
 #The code below still needs to be tranlated in flask-sqlalchemy
 
-@app.route('/todo/<int:task_id>')
-def index(task_id):
-    return jsonify(greeting="<h1> Hello Task Id # {}  </h1>".format(task_id))
+#@app.route('/todo/<int:task_id>')
+#def index(task_id):
+#    return jsonify(greeting="<h1> Hello Task Id # {}  </h1>".format(task_id))
 
-@app.route('/todo', methods = ['GET'])
+#@app.route('/todo', methods = ['GET'])
+@app.route('/heroes', methods = ['GET'])
 def get_all_tasks():
     Session = sessionmaker(bind=engine)
     Session.configure()
     session = Session()
-    results = Todo.query.filter_by(done=False).all()
+    #results = Todo.query.filter_by(done=False).all()
+    heroes = Hero.query.all()
     reslist = []
     for row in results:
-        reslist.append(dict(id=row.id, task=row.task, priority=row.priority, due = row.due.isoformat()))
+        #reslist.append(dict(id=row.id, task=row.task, priority=row.priority, due = row.due.isoformat()))
+        reslist.append(dict(name=row.name))
 
     print(reslist)
     return jsonify(tasklist=reslist)
 
+"""
 @app.route('/todo/<int:task_id>', methods = ['PUT'])
 def todo_one(task_id):
     Session = sessionmaker(bind=engine)
@@ -74,6 +93,7 @@ def todo_one(task_id):
     row = Todo.query.filter_by(id=task_id).update(dict(done=True))
     session.commit()
     return "done"
+"""
 
 if __name__ == '__main__':
     app.run()
