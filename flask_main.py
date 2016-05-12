77dda65b04228d1c6ba3d5eb7c49536238dcc1fe
austinnash <nashau01@luther.edu>
2016-05-12 09:34:12 -0500

    debugging heroku deployment

--- flask_main.py
+++ flask_main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from model.fp_flask_sql import *
+# from model.fp_flask_sql import *
 
 flask_app = Flask(__name__)
 flask_app.debug = True
@@ -18,16 +18,16 @@ def render_a_template():
     return render_template('drafthelper.html', foo='')
 
 
-#@flask_app.route('/todo', methods = ['GET'])
-@flask_app.route('/heroes', methods = ['GET'])
-def get_all_heroes():
-    heroes = Hero.query.all()
-    reslist = []
-    for row in results:
-        reslist.append(dict(name=row.name))
-
-    print(reslist)
-    return jsonify(heroes=reslist)
+# #@flask_app.route('/todo', methods = ['GET'])
+# @flask_app.route('/heroes', methods = ['GET'])
+# def get_all_heroes():
+#     heroes = Hero.query.all()
+#     reslist = []
+#     for row in results:
+#         reslist.append(dict(name=row.name))
+#
+#     print(reslist)
+#     return jsonify(heroes=reslist)
 
 """
 @flask_app.route('/todo/<int:task_id>', methods = ['PUT'])
